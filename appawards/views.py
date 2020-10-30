37a70fe49de4d35855c9cd02e47bfd6908d9d851
jessevictor <kongoijesse97@gmail.com>
2020-10-30 17:37:34 +0300

    add views

--- appawards/views.py
+++ appawards/views.py
@@ -1,11 +1,15 @@
 from django.http  import HttpResponse,Http404
 from django.shortcuts import render, redirect
-from .forms import NewPostForm, UserUpdateForm, ProfileUpdateForm
-from .models import Image
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import  CreateUserForm,NewPostForm, UserUpdateForm, ProfileUpdateForm
+from .models import Image
 
 
-@login_required(login_url='/accounts/login/')
+@login_required(login_url='registration/login/')
+
 def home(request):
     all_posts = Image.all_images()
     if request.method == 'POST':
@@ -16,7 +20,7 @@ def home(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            # messages.success(request, f'your profile has been updated')
+            messages.success(request, f'your profile has been updated')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
@@ -29,6 +33,52 @@ def home(request):
     }
     return render(request, 'index.html', context)
 
+def loginPage(request):
+    if request.user.is_authenticatd:
+        return redirect('/')
+    else:
+    	if request.user.is_authenticated:
+            if request.method == 'POST':
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+            else:                                                               
+                messages.info(request,'Username OR password is incorrect')	
+            context = {}
+            return render(request,'login.html',context)
+        
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profile.html', context)
+
 def new_post(request):
     if request.method == 'POST':
         form = NewPostForm(request.POST, request.FILES)
@@ -37,30 +87,20 @@ def new_post(request):
             image = form.save(commit = False)
             image.image_poster = request.user
             image.save()
-        return redirect('index')
+        return redirect('/')
 
     else:
         form = NewPostForm()
     return render(request, 'post_image.html', {'form': form})
 
-def profile(request):
+def registerPage(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance = request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                 request.FILES, 
-                                 instance= request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            # messages.success(request, f'your profile has been updated')
-            return redirect('profile')
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created successfully!')
+            return redirect('login')
     else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
-    return render(request, 'profile.html', context)
+        form = CreateUserForm()
+    return render(request, 'registration/register.html', {'form': form})
