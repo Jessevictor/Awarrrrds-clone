a0affcebf5569dbd2f493bb4b91428206eb1711d
jessevictor <kongoijesse97@gmail.com>
2020-10-30 17:36:39 +0300

    add views

---
+++ appawards/forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import  Profile, Image
+from django.contrib.auth.models import User
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields =['username','email','password1','password2']
+                                  
+class NewPostForm(forms.ModelForm):
+    '''
+    Form for a user to create a new form
+    '''
+    class Meta:
+        model = Image
+        exclude = ['image_poster']
+  
+class ProfileUpdateForm(forms.ModelForm):
+      class Meta:
+          model = Profile
+          fields = ['image']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    bio = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','bio']
