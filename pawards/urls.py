d1842212e429d07b120f69406947cdaee5999078
jessevictor <kongoijesse97@gmail.com>
2020-10-31 13:30:51 +0300

    Deployment to heroku

--- pawards/urls.py
+++ pawards/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin 
 from django.conf.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('appawards.urls')),
